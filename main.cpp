cb998c433e6d6c9dea478642533952e71bfe65d9
Oleg Potkin <olpotkin@gmail.com>
2017-09-05 13:08:33 +0300

    style: Minor corrections

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 vector<vector<double>> Load_State(string file_name) {
-	ifstream in_state_(file_name.c_str(), ifstream::in);
+  ifstream in_state_(file_name.c_str(), ifstream::in);
   vector<vector<double>> state_out;
   string line;
 
@@ -18,7 +18,7 @@ vector<vector<double>> Load_State(string file_name) {
     while(getline(iss, token, ',')) {
       x_coord.push_back(stod(token));
     }
-    	state_out.push_back(x_coord);
+    state_out.push_back(x_coord);
   }
 
   return state_out;
@@ -43,34 +43,44 @@ vector<string> Load_Label(string file_name) {
 
 
 int main() {
+  // Each coordinate contains 4 pieces of information:
+  // s - distance along the road (longitudinal displacement)
+  // d - side-to-side position on the road (lateral displacement)
+  // s_dot - delta for longitudinal displacement
+  // d_dot - delta for lateral displacement
   vector<vector<double>> X_train = Load_State("./pred_data/train_states.txt");
   vector<vector<double>> X_test  = Load_State("./pred_data/test_states.txt");
+
+  // Action:
+  // - left
+  // - right
+  // - keep
   vector<string> Y_train  = Load_Label("./pred_data/train_labels.txt");
   vector<string> Y_test   = Load_Label("./pred_data/test_labels.txt");
 
-	cout << "X_train number of elements " << X_train.size() << endl;
-	cout << "X_train element size " << X_train[0].size() << endl;
-	cout << "Y_train number of elements " << Y_train.size() << endl;
+  cout << "X_train number of elements " << X_train.size() << endl;
+  cout << "X_train element size " << X_train[0].size() << endl;
+  cout << "Y_train number of elements " << Y_train.size() << endl;
 
-	GNB gnb = GNB();
+  GNB gnb = GNB();
 
-	gnb.train(X_train, Y_train);
+  gnb.train(X_train, Y_train);
 
-	cout << "X_test number of elements " << X_test.size() << endl;
-	cout << "X_test element size " << X_test[0].size() << endl;
-	cout << "Y_test number of elements " << Y_test.size() << endl;
+  cout << "X_test number of elements " << X_test.size() << endl;
+  cout << "X_test element size " << X_test[0].size() << endl;
+  cout << "Y_test number of elements " << Y_test.size() << endl;
 
-	int score = 0;
-	for(int i = 0; i < X_test.size(); i++) {
-		vector<double> coords = X_test[i];
-		string predicted = gnb.predict(coords);
-		if(predicted.compare(Y_test[i]) == 0) {
-			score += 1;
-		}
-	}
+  int score = 0;
+  for(int i = 0; i < X_test.size(); i++) {
+    vector<double> coords = X_test[i];
+    string predicted = gnb.predict(coords);
+    if(predicted.compare(Y_test[i]) == 0) {
+      score += 1;
+    }
+  }
 
-	float fraction_correct = float(score) / Y_test.size();
-	cout << "You got " << (100 * fraction_correct) << " correct" << endl;
+  float fraction_correct = float(score) / Y_test.size();
+  cout << "You got " << (100 * fraction_correct) << " correct" << endl;
 
-	return 0;
+  return 0;
 }
