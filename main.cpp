aea3fc857ddb31208858caa00abdf8c967ee1ad9
Oleg Potkin <olpotkin@gmail.com>
2017-09-06 11:51:48 +0300

    feat: remove 's' feature from dataset
    Prediction accuracy increased (from 65.4 to 70.8)

--- main.cpp
+++ main.cpp
@@ -48,8 +48,27 @@ int main() {
   // d - side-to-side position on the road (lateral displacement)
   // s_dot - velocity of the car along the s axis (for longitudinal displacement)
   // d_dot - velocity of the car along the d axis (for lateral displacement)
-  vector<vector<double>> X_train = Load_State("./pred_data/train_states.txt");
-  vector<vector<double>> X_test  = Load_State("./pred_data/test_states.txt");
+  vector<vector<double>> X_train_f = Load_State("./pred_data/train_states.txt");
+  vector<vector<double>> X_test_f  = Load_State("./pred_data/test_states.txt");
+
+  // Remove feature [0] 's' from training set
+  int train_dim_1 = X_train_f.size();
+  int train_dim_2 = X_train_f[0].size();
+  vector<vector<double>> X_train(train_dim_1, vector<double>(train_dim_2 - 1));
+  for (size_t i = 0; i < train_dim_1; ++i) {
+    for (size_t j = 1; j < train_dim_2; ++j) {
+      X_train[i][j-1] = X_train_f[i][j];
+    }
+  }
+  // Remove feature [0] 's' from test set
+  int test_dim_1 = X_test_f.size();
+  int test_dim_2 = X_test_f[0].size();
+  vector<vector<double>> X_test(test_dim_1, vector<double>(test_dim_2 - 1));
+  for (size_t i = 0; i < test_dim_1; ++i) {
+    for (size_t j = 1; j < test_dim_2; ++j) {
+      X_test[i][j-1] = X_test_f[i][j];
+    }
+  }
 
   // Action:
   // - left
